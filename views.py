44ac3c6c9fb7f53f3b6119223e1ba9e51332809e
Salnikov Alex <salnikov17a@gmail.com>
2024-09-05 21:51:32 +0300

    refactored views.py

--- views.py
+++ views.py
@@ -4,11 +4,13 @@ from aiogram.types import FSInputFile, KeyboardButton, ReplyKeyboardMarkup
 
 
 def getWelcomeMessage(message: Message) -> Message:
-    return message.answer(f"""
-        Привет, {message.from_user.first_name}! Добро пожаловать в симулятор торговли на бирже!\n\nЗдесь ты можешь отточить свои навыки торговли, провепить новые стратегии или просто развлечься.\nВсе котировки акций реальны, взяты с MOEX
-    """)
+    answer = "Привет, {message.from_user.first_name}! Добро пожаловать в симулятор торговли на бирже!\n\n" \
+    "Здесь ты можешь отточить свои навыки торговли, провепить новые стратегии или просто развлечься.\n" \
+    "Все котировки акций реальны, взяты с MOEX"
+    return message.answer(answer)
 
-def getQuotesList(message: Message, shares, share_company_names) -> Message:
+
+def getQuotesList(message: Message, shares: list, share_company_names: list) -> Message:
     answer = "Список котировок биржы:\n\n"
     for share in shares:
         growEmoji =  "🟢" if share.isRising else "🔴"
@@ -16,14 +18,18 @@ def getQuotesList(message: Message, shares, share_company_names) -> Message:
     answer += "\n"
     return message.answer(answer)
 
-def getMainMenuMessage(message: Message, shares) -> Message:
-    answer = "Ваш счёт:\nЦена ваших акций:\n\nРастущие акции: \n"
+
+def getMainMenuMessage(message: Message, shares: list) -> Message:
+    answer = "Ваш счёт:\n" \
+    "Цена ваших акций:" \
+    "\n\nРастущие акции: \n"
     for share in shares:
         if share.isRising:
             answer += " " + share.company_name + ": " + str(share.actual_price) + " рублей\n"
     return message.answer(answer)
 
-def getShareInfo(message: Message, company_name, shares) -> Message:
+
+def getShareInfo(message: Message, company_name: str, shares: list) -> Message:
     share = next(filter(lambda x: x.company_name == company_name, shares))
     line_chart_file =  FSInputFile(f"line_charts/{share.code}.png")
     return message.answer_photo(line_chart_file, caption=f"Цена 1 акции: {share.actual_price} рублей")
