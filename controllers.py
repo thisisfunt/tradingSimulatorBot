2b7b1b82d73c284a7b760064a6a075ae246cd082
Salnikov Alex <salnikov17a@gmail.com>
2024-09-06 12:59:25 +0300

    changed names of view functions to pep8

--- controllers.py
+++ controllers.py
@@ -12,13 +12,13 @@ dispatcher = Dispatcher()
 async def command_start_handler(message: Message) -> None:
     tg_id = message.from_user.id
     models.create_user_if_not_exist(tg_id)
-    await views.getWelcomeMessage(message)
+    await views.get_welcome_message(message)
 
 @dispatcher.message()
 async def message_handler(message: Message) -> None:
     if message.text == "Счёт":
-        await views.getAmountMessage(message, models.shares)
+        await views.get_amount_message(message, models.shares)
     elif message.text == "Список акций":
-        await views.getQuotesList(message, models.shares)
+        await views.get_quotes_list(message, models.shares)
     elif message.text in models.share_company_names:
-        await views.getShareInfo(message, message.text, models.shares)
+        await views.get_share_info(message, message.text, models.shares)
--- views.py
+++ views.py
@@ -3,14 +3,14 @@ from aiogram import html
 from aiogram.types import FSInputFile, KeyboardButton, ReplyKeyboardMarkup
 
 
-def getWelcomeMessage(message: Message) -> Message:
+def get_welcome_message(message: Message) -> Message:
     answer = f"Привет, {message.from_user.first_name}! Добро пожаловать в симулятор торговли на бирже!\n\n" \
     "Здесь ты можешь отточить свои навыки торговли, провепить новые стратегии или просто развлечься.\n" \
     "Все котировки акций реальны, взяты с MOEX"
     return message.answer(answer)
 
 
-def getAmountMessage(message: Message, shares: list) -> Message:
+def get_amount_message(message: Message, shares: list) -> Message:
     answer = "Ваш счёт:\n" \
     "Цена ваших акций:" \
     "\n\nРастущие акции: \n"
@@ -20,7 +20,7 @@ def getAmountMessage(message: Message, shares: list) -> Message:
     return message.answer(answer)
 
 
-def getQuotesList(message: Message, shares: list) -> Message:
+def get_quotes_list(message: Message, shares: list) -> Message:
     answer = "Список котировок биржы:\n\n"
     for share in shares:
         growEmoji =  "🟢" if share.isRising else "🔴"
@@ -29,7 +29,7 @@ def getQuotesList(message: Message, shares: list) -> Message:
     return message.answer(answer)
 
 
-def getShareInfo(message: Message, company_name: str, shares: list) -> Message:
+def get_share_info(message: Message, company_name: str, shares: list) -> Message:
     share = next(filter(lambda x: x.company_name == company_name, shares))
     line_chart_file =  FSInputFile(f"line_charts/{share.code}.png")
     return message.answer_photo(line_chart_file, caption=f"Цена 1 акции: {share.actual_price} рублей")
